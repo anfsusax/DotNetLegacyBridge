35e9010560a14adaa6fcd3a13d20ca8702ebb414
anfsusax <anfsusax@gmail.com>
2025-11-06 11:46:24 -0300

    Correção de errod de configiraçã owebForms

--- GET.WebForms/Global.asax.cs
+++ GET.WebForms/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Web.UI;
 
 namespace GET.WebForms
 {
@@ -16,6 +17,17 @@ namespace GET.WebForms
             // Código que é executado na inicialização do aplicativo
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+            // Configurar ScriptResourceMapping para jQuery (necessário para UnobtrusiveValidationMode)
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery-3.4.1.min.js",
+                DebugPath = "~/Scripts/jquery-3.4.1.js",
+                CdnPath = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js",
+                CdnDebugPath = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.js",
+                CdnSupportsSecureConnection = true,
+                LoadSuccessExpression = "window.jQuery"
+            });
         }
     }
 }
